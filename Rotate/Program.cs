cb9c257a3ecf25897f1e61d772f0fc85333f71dc
Ekta kumari <95992272+Ekta-Coder1997@users.noreply.github.com>
2021-12-29 15:47:28 +0530

    Add files via upload

---
+++ Rotate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Rotate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            int position = 0, k = 0;
+            Console.Write("Original Array :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Enter position of rotation : ");
+            position = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            while (k < position)
+            {
+                obj.LeftRotate(arr);
+                k++;
+            }
+            Console.WriteLine("Rotation of array by position " + position);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+        void LeftRotate(int[] arr)
+        {
+            int x = arr[0];
+            for (int i = 0; i < (arr.Length - 1); i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[(arr.Length - 1)] = x;
+        }
+    }
+}
